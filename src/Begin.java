db46be405cf794fb2a49976b35e86b040fa85ce7
15901893257 <1372565062@qq.com>
2016-12-17 23:46:02 +0800

    Add files via upload

---
+++ src/Begin.java
@@ -0,0 +1,91 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Begin extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Begin frame = new Begin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Begin() {
+		setTitle("\u6B22\u8FCE\u767B\u9646\u533B\u9662\u7BA1\u7406\u7CFB\u7EDF");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("\u8BF7\u9009\u62E9\u767B\u9646");
+		lblNewLabel.setFont(new Font("ËÎÌו", Font.PLAIN, 18));
+		lblNewLabel.setBounds(146, 0, 102, 35);
+		contentPane.add(lblNewLabel);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("\u9662\u957F");
+		rdbtnNewRadioButton.setBounds(38, 45, 121, 23);
+		contentPane.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("\u7BA1\u7406\u5458");
+		rdbtnNewRadioButton_1.setBounds(38, 76, 121, 23);
+		contentPane.add(rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("\u533B\u751F");
+		rdbtnNewRadioButton_2.setBounds(38, 107, 121, 23);
+		contentPane.add(rdbtnNewRadioButton_2);
+		
+		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("\u836F\u5E08");
+		rdbtnNewRadioButton_3.setBounds(38, 138, 121, 23);
+		contentPane.add(rdbtnNewRadioButton_3);
+		
+		JRadioButton rdbtnNewRadioButton_4 = new JRadioButton("\u6536\u8D39\u4EBA\u5458");
+		rdbtnNewRadioButton_4.setBounds(38, 169, 121, 23);
+		contentPane.add(rdbtnNewRadioButton_4);
+		
+		ButtonGroup bg=new ButtonGroup();
+		bg.add(rdbtnNewRadioButton);
+		bg.add(rdbtnNewRadioButton_1);
+		bg.add(rdbtnNewRadioButton_2);
+		bg.add(rdbtnNewRadioButton_3);
+		bg.add(rdbtnNewRadioButton_4);
+
+		JButton btnNewButton = new JButton("\u786E\u8BA4");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(rdbtnNewRadioButton.isSelected()){
+					PLogin pl=new PLogin();
+				}
+			}
+		});
+		btnNewButton.setBounds(35, 205, 78, 35);
+		contentPane.add(btnNewButton);
+	}
+}
